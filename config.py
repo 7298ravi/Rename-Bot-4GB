fa05b8cb776847b9034a8c5d19b12661cc5c37cf
7298ravi <RDJHindiofficialrg@gmail.com>
2024-10-03 09:36:15 +0530

    Config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "25613025"))
+API_HASH = os.environ.get("API_HASH", "b9c705fb515c81e90f239918f2f7f668")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7848450856:AAGzCfGnAE71YU7VwJ5vm-3DLCDFgJM7Y54")
+ADMIN = int(os.environ.get("ADMIN", "6620837238"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002409372193")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://LEGEND_OF_ALL_TIME:4lD7yH5HwL0ONHwq@legend.giyqu.mongodb.net/?retryWrites=true&w=majority&appName=LEGEND")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "RdjUploaderbot")
 
 
 # Other Variables Config
@@ -32,7 +32,7 @@ START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d
 
 # If You Need To Add Verify System Then Message Me On Telegram
 # Check Demo Bots
-# https://t.me/FileRenameXBot
+# https://t.me/RdjUploaderbot
 # https://t.me/PremiumRenamerRobot
 # https://t.me/FileRenamerXRobot
 # Token Verification Adding Features Is Paid So If You Want Then Dm Me
@@ -47,6 +47,6 @@ START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
+# Telegram Channel @Animes_hindi_dubbed_series
+# Backup Channel @Crunchyroll_Official_Dubbed
 # Developer @JishuDeveloper
